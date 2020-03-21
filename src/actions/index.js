443bb0e6daf19cd02f4e96de659e0e420d72aa83
vu <nam>
2020-03-21 12:13:30 +0700

    change index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,4 +11,4 @@ export const counterIncrease = () => dispatch => {
             }); 
 };
 
-export const counterDecrease = () => ({ type: DECREASE });zzdddf
+export const counterDecrease = () => ({ type: DECREASE });
